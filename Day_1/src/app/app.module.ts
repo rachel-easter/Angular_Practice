87136bb3b18f26195875b7e2f7be828078d9c0f4
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2024-01-02 00:45:37 +0530

    Create app.module.ts

---
+++ Day_1/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+
+@NgModule({
+  declarations: [AppComponent, SearchBarComponent, SearchResultsComponent],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
