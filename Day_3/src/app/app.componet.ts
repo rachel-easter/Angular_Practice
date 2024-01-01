e8a50cea0aa80ae2955237ef6b260d4f89ed24f9
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2024-01-02 00:56:20 +0530

    Create app.componet.ts

---
+++ Day_3/src/app/app.componet.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>
+      <h1>Amazon-Like Wish List</h1>
+      <app-wish-list></app-wish-list>
+    </div>
+  `,
+})
+export class AppComponent {}
