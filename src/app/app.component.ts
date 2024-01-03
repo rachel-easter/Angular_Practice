84f04ad2abb9d02fc01b879d32e7a5ce655ffdd7
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2024-01-03 17:06:53 +0530

    Day_5
    Topic_pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CurrencyService } from './currency.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'notepad';
+  title = '';
+  amount!: number;
+  fromCurrency!: string ;
+  toCurrency!: string ;
+  conversionRate!: number;
+  conversionResult: number|undefined;
+  currencies = [
+    { name: 'US Dollar', code: 'USD' },
+    { name: 'Euro', code: 'EUR' },
+   
+  ];
+
+  constructor(private currencyService: CurrencyService) {}
+
+  convertCurrency() {
+    if (this.amount && this.fromCurrency && this.toCurrency) {
+      this.currencyService.getConversionRate(this.fromCurrency, this.toCurrency)
+        .subscribe({         
+          next: rate => {
+            this.conversionRate = rate;
+            this.calculateConversionResult();
+          },
+          error: error => {
+            console.error('Error converting currency:', error);
+            // Handle error as needed
+            this.conversionResult = undefined; // Reset result in case of error
+          }
+    });
+    }
+  }
+
+  private calculateConversionResult() {
+    this.conversionResult = this.amount && this.conversionRate ? this.amount * this.conversionRate : undefined;
+  }
 }
